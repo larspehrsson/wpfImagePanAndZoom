0d2044b2d23bc6dc48a960d1e2c7d2b6b2cbcc4c
Lars Pehrsson <lars@allthingsbeep.com>
2020-08-02 17:36:08 +1000

    Migrated from CodeProject

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace WPFImagePanAndZoom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point origin;
+        private Point start;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            WPFWindow.MouseWheel += MainWindow_MouseWheel;
+
+            image.MouseLeftButtonDown += image_MouseLeftButtonDown;
+            image.MouseLeftButtonUp += image_MouseLeftButtonUp;
+            image.MouseMove += image_MouseMove;
+        }
+
+        private void image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            image.ReleaseMouseCapture();
+        }
+
+        private void image_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!image.IsMouseCaptured) return;
+            Point p = e.MouseDevice.GetPosition(border);
+
+            Matrix m = image.RenderTransform.Value;
+            m.OffsetX = origin.X + (p.X - start.X);
+            m.OffsetY = origin.Y + (p.Y - start.Y);
+
+            image.RenderTransform = new MatrixTransform(m);
+        }
+
+        private void image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (image.IsMouseCaptured) return;
+            image.CaptureMouse();
+
+            start = e.GetPosition(border);
+            origin.X = image.RenderTransform.Value.OffsetX;
+            origin.Y = image.RenderTransform.Value.OffsetY;
+        }
+
+        private void MainWindow_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(image);
+
+            Matrix m = image.RenderTransform.Value;
+            if (e.Delta > 0)
+                m.ScaleAtPrepend(1.1, 1.1, p.X, p.Y);
+            else
+                m.ScaleAtPrepend(1/1.1, 1/1.1, p.X, p.Y);
+
+            image.RenderTransform = new MatrixTransform(m);
+        }
+    }
+}
